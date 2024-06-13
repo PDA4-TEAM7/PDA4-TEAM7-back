a7556269a365948ff9ea71fd24cdd78d055a9060
sooyeon-kr <sooyeon7027@naver.com>
2024-06-13 11:02:15 +0900

    feat: 서버 새로 시작 시, 모델을 새로 생성하지 않도록 force:true를 false로 변경

--- src/models/index.ts
+++ src/models/index.ts
@@ -47,7 +47,7 @@ export const initializeDatabase = async () => {
     try {
         await sequelize.authenticate();
         console.log('Connection has been established successfully.');
-        await sequelize.sync({ force: true }); // 데이터베이스와 모델 동기화
+        await sequelize.sync({ force: false }); // 데이터베이스와 모델 동기화
         console.log('Database synchronized successfully.');
     } catch (error) {
         console.error('Unable to connect to the database:', error);
