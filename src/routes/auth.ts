ed3f7c585c0a070d2d488b65753f3f155b8dcb1f
sooyeon-kr <sooyeon7027@naver.com>
2024-06-14 19:35:59 +0900

    : 회원가입 시 빈칸 및 중복처리 요청 로직 추가 작성

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,5 +1,5 @@
-import express from 'express';
-import { signUp } from '../controllers/auth';
+import express from "express";
+import { signUp } from "../controllers/auth";
 export default (router: express.Router) => {
-    router.post('/auth/signup', signUp);
+  router.post("/auth/signup", signUp);
 };
