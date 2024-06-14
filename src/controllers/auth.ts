5c35135907fdec671a4cdddfde3ba5ecf8850f24
sooyeon-kr <sooyeon7027@naver.com>
2024-06-14 19:35:59 +0900

    : 회원가입 시 빈칸 및 중복처리 요청 로직 추가 작성

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -4,23 +4,23 @@ import validateAPI from "../services/validateAPI";
 
 export const signUp = async (req: Request, res: Response) => {
   try {
-    const auth = req.body;
+    const auth = req.body.auth;
 
     const [isUserIdAvailable, isUsernameAvailable] = await Promise.all([
-      validateAPI.checkUserIdAvailability(auth.user_id),
-      validateAPI.checkUsernameAvailability(auth.username),
+      validateAPI.checkUserIdAvailability(auth.user_id as string),
+      validateAPI.checkUsernameAvailability(auth.username as string),
     ]);
+
     if (!isUserIdAvailable) {
-      return res.status(409).json({ message: "이미 사용중인 사용자 ID입니다." });
+      return res.json({ message: "이미 사용중인 사용자 ID입니다." });
     }
     if (!isUsernameAvailable) {
-      return res.status(409).json({ message: "이미 사용중인 사용자명입니다." });
+      return res.json({ message: "이미 사용중인 사용자명입니다." });
     }
-
     const user = await authAPI.signUp(auth);
-    return res.status(200).json({ user: user });
+    return res.status(200).json({ user: user, message: "회원가입 성공!" });
   } catch (error) {
-    res.status(500).json({ message: "회원가입 실패", error: error });
+    res.status(500).json({ message: "회원가입 실패!", error: error });
     console.error(error);
   }
 };
--- src/services/validateAPI.ts
+++ src/services/validateAPI.ts
@@ -1,20 +1,29 @@
-import { User } from '../models/user';
+import { User } from "../models/user";
 
 class validateAPI {
-    // 아이디 중복 검사
-    static async checkUserIdAvailability(user_id: string): Promise<boolean> {
-        const user = await User.findOne({
-            where: { user_id: user_id },
-        });
-        return !user;
+  // 아이디 중복 검사
+  static async checkUserIdAvailability(user_id: string): Promise<boolean> {
+    console.log("checkIn", user_id);
+    if (!user_id || user_id.trim() === "") {
+      console.log("요기?");
+      return false;
     }
+    const user = await User.findOne({
+      where: { user_id: user_id },
+    });
+    return user === null ? true : false;
+  }
 
-    static async checkUsernameAvailability(username: string): Promise<boolean> {
-        const user = await User.findOne({
-            where: { username: username },
-        });
-        return !user;
+  static async checkUsernameAvailability(username: string): Promise<boolean> {
+    if (!username || username.trim() === "") {
+      return false;
     }
+    const user = await User.findOne({
+      where: { username: username },
+    });
+
+    return user === null ? true : false;
+  }
 }
 
 export default validateAPI;
