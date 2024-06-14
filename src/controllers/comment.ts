11e7f9539683b9fdb86e496c55c595f4526b9d66
Elandland <bodan99@gmail.com>
2024-06-14 09:59:01 +0900

    Feat. comment update api 추가

--- src/controllers/comment.ts
+++ src/controllers/comment.ts
@@ -16,9 +16,7 @@ export const writeComment = async (req: Request, res: Response) => {
     const portfolio = await Portfolio.findByPk(dummyPortfolioId);
 
     if (!user || !portfolio) {
-      return res
-        .status(404)
-        .json({ message: "해당 포트폴리오나 유저가 없습니다." });
+      return res.status(404).json({ message: "해당 포트폴리오나 유저가 없습니다." });
     }
 
     const newComment = await Comment.create({
@@ -84,6 +82,30 @@ export const deleteComment =  async (req: Request, res: Response) => {
     res.status(200).json({ message: "댓글이 삭제되었습니다." });
   } catch (error) {
     console.error("에러발생:", error);
+    res.status(500).json({ message: "Server error", error });
+  }
+};
+
+// Comment 업데이트 API
+export const updateComment = async (req: Request, res: Response) => {
+  const { comment_id } = req.params;
+  const { description } = req.body;
+
+  try {
+    const comment = await Comment.findByPk(comment_id);
+    if (!comment) {
+      return res.status(404).json({ message: "댓글이 존재하지 않습니다." });
+    }
+
+    // 업데이트 할 데이터 설정
+    comment.description = description;
+    comment.update_dt = new Date();
+    comment.is_update = true;
+
+    await comment.save();
+    res.status(200).json({ message: "댓글이 업데이트되었습니다.", comment });
+  } catch (error) {
+    console.error("에러 발생:", error);
     res.status(500).json({ message: "Server error", error });
   }
 };
--- src/routes/comment.ts
+++ src/routes/comment.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { deleteComment, readComment, writeComment } from "../controllers/comment";
+import { deleteComment, readComment, writeComment,updateComment } from "../controllers/comment";
 
 export default (router: express.Router) => {
   //컨트롤러연결
@@ -8,4 +8,6 @@ export default (router: express.Router) => {
   router.get("/comment/read/:portfolio_id", readComment)
 
   router.get("/comment/delete/:comment_id",deleteComment)
+
+  router.patch("/comment/update/:comment_id",updateComment)
 };
