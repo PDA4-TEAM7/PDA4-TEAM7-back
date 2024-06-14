29197220b265eaa0e7de78b4758962288adc679f
sooyeon-kr <sooyeon7027@naver.com>
2024-06-14 09:38:21 +0900

    feat : cors 허용 설정 - 개발단계이므로 전체 도메인에 대해 허용함

--- src/index.ts
+++ src/index.ts
@@ -1,53 +1,53 @@
-import express, { Request, Response, NextFunction } from "express";
-import cors from "cors";
-import config from "./config/index.config";
-import { initializeDatabase } from "./models/index";
-import { stockAccountApi } from "./services/stockAccountAPI";
-import router from "./routes/index";
+import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
+import config from './config/index.config';
+import { initializeDatabase } from './models/index';
+import { stockAccountApi } from './services/stockAccountAPI';
+import router from './routes/index';
 const app = express();
 
 // 기본 미들웨어
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
-
+app.use(cors({ origin: '*', credentials: true }));
+// app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 // 루트 라우트
-app.get("/", (req: Request, res: Response) => {
-  res.send("Welcome to the API");
+app.get('/', (req: Request, res: Response) => {
+    res.send('Welcome to the API');
 });
 
-app.use("/api", router());
+app.use('/api', router());
 
 //DUMMY : 한투 테스트용 api 나중에 지울예정
-app.get("/me", async (req: Request, res: Response) => {
-  try {
-    const hantuService = new stockAccountApi();
-    const data = await hantuService.inquireBalance();
-    console.log(data);
-    res.send(data);
-  } catch (err) {
-    console.log(err);
-    res.send(`fail : ${err}`);
-  }
+app.get('/me', async (req: Request, res: Response) => {
+    try {
+        const hantuService = new stockAccountApi();
+        const data = await hantuService.inquireBalance();
+        console.log(data);
+        res.send(data);
+    } catch (err) {
+        console.log(err);
+        res.send(`fail : ${err}`);
+    }
 });
 
 // 에러 핸들링 미들웨어
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-  console.error(err.stack);
-  res.status(500).send("Something broke!");
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
 });
 
 // 서버 시작 함수
 const startServer = async () => {
-  app.listen(config.port, async () => {
-    try {
-      await initializeDatabase(); // 모델 초기화와 동기화
-    } catch (error) {
-      console.error("Server startup failed:", error);
-      return; // 데이터베이스 초기화 실패시 서버를 시작하지 않음
-    }
-    console.log(`Server running on http://localhost:${config.port}`);
-  });
+    app.listen(config.port, async () => {
+        try {
+            await initializeDatabase(); // 모델 초기화와 동기화
+        } catch (error) {
+            console.error('Server startup failed:', error);
+            return; // 데이터베이스 초기화 실패시 서버를 시작하지 않음
+        }
+        console.log(`Server running on http://localhost:${config.port}`);
+    });
 };
 
 export { startServer };
