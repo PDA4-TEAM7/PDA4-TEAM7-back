6e33a8fc3ae285317b5f43557c48164062da17dc
Elandland <bodan99@gmail.com>
2024-06-16 15:12:19 +0900

    Feat. comment에서 dummy로 변수명 받던거 변경

--- src/routes/comment.ts
+++ src/routes/comment.ts
@@ -1,13 +1,18 @@
 import express from "express";
-import { deleteComment, readComment, writeComment,updateComment } from "../controllers/comment";
+import {
+  deleteComment,
+  readComment,
+  writeComment,
+  updateComment,
+} from "../controllers/comment";
 
 export default (router: express.Router) => {
   //컨트롤러연결
   router.post("/comment/write", writeComment);
 
-  router.get("/comment/read/:portfolio_id", readComment)
+  router.get("/comment/read/:portfolio_id", readComment);
 
-  router.delete("/comment/delete/:comment_id",deleteComment)
+  router.delete("/comment/delete/:comment_id", deleteComment);
 
-  router.patch("/comment/update/:comment_id",updateComment)
+  router.patch("/comment/update/:comment_id", updateComment);
 };
