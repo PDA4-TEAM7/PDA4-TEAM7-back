7ad8185184b75d67dc3dab2093146bda00fdfffe
soyeon <uyyu1734@gmail.com>
2024-06-18 16:52:06 +0900

    Revert "fix: user_id 타입추가하고 코드 수정"
    This reverts commit 13d4891197ae958a1bd2b6a5b0b5888ac5d805cb.

--- src/types/express/index.d.ts
+++ src/types/express/index.d.ts
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 declare global {
   namespace Express {
     interface Request {
-      user?: { uid: string; username: string; user_id: string };
+      user?: { uid: string; username: string };
     }
   }
 }
