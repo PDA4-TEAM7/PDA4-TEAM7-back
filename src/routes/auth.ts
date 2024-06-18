72f905b15ae53a4f8be01e7f863ccf79c960df17
sooyeon-kr <sooyeon7027@naver.com>
2024-06-18 15:10:12 +0900

    Feat: 유저가 로그인되어 있는지 확인하는 API 작성 POST /auth/islogin

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,7 +1,9 @@
 import express from "express";
-import { signUp, signIn, signOut } from "../controllers/auth";
+import requireAuthMiddleware from "../middleware/requireAuthMiddleware";
+import { signUp, signIn, signOut, isLogin } from "../controllers/auth";
 export default (router: express.Router) => {
   router.post("/auth/signup", signUp);
   router.post("/auth/signin", signIn);
-  router.post("/auth/signout", signOut);
+  router.post("/auth/signout", requireAuthMiddleware, signOut);
+  router.post("/auth/islogin", requireAuthMiddleware, isLogin);
 };
