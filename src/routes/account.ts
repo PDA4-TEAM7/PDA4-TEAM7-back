170a5dd9744422eb7fc4eaa3988b5058c24b0972
sooyeon-kr <sooyeon7027@naver.com>
2024-06-21 16:19:31 +0900

    Feat: requireAuthMiddleware 추가 - 자신의 계좌 추가 - 자신의 계좌 리스트 보기 - 인증된 사용자만 기능을 사용할 수 있도록

--- src/routes/account.ts
+++ src/routes/account.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import { setAccount, getAccount, getMyAccountList, deleteAccount } from "../controllers/account";
+import requireAuthMiddleware from "../middleware/requireAuthMiddleware";
 
 export default (router: express.Router) => {
   //account추가
-  router.post("/account", setAccount);
+  router.post("/account", requireAuthMiddleware, setAccount);
   router.get("/account/:accountId", getAccount);
-  router.get("/accountlist", getMyAccountList);
-  router.delete("/account/:accountId", deleteAccount);
+  router.get("/accountlist", requireAuthMiddleware, getMyAccountList);
+  router.delete("/account/:accountId", requireAuthMiddleware, deleteAccount);
 };
