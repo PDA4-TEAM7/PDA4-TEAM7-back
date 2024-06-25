98d2d6ff61ecab2257f56b1ee76429fe7927cc66
sooyeon-kr <sooyeon7027@naver.com>
2024-06-25 22:46:50 +0900

    Feat: 구독한 포트폴리오의 거래 변동 내역을 가져오는 기능 추가 - getMySubRecencyTradingHistory

--- src/controllers/recency.ts
+++ src/controllers/recency.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { UpdateRecencyHoldingAPI } from "../services/updateRecencyHoldingAPI";
+import recencyAPI from "../services/recencyAPI";
 export const updateAllHoldings = async (req: Request, res: Response) => {
   try {
     await UpdateRecencyHoldingAPI.updateAllHoldings();
@@ -13,6 +14,17 @@ export const updateAllHistory = async (req: Request, res: Response) => {
   try {
     await UpdateRecencyHoldingAPI.updateAllHistory();
     res.status(200).json("최근 거래 내역:updateAllHistory 업데이트 완료");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getMySubRecencyTradingHistory = async (req: Request, res: Response) => {
+  const { uid } = (req as any).user;
+  try {
+    const mySubRecencyTradingHistory = await recencyAPI.getMySubRecencyTradingHistory(uid);
+    console.log("mySubRecencyTradingHistory", mySubRecencyTradingHistory);
+    res.status(200).json(mySubRecencyTradingHistory);
   } catch (error) {
     console.log(error);
   }
--- src/routes/recency.ts
+++ src/routes/recency.ts
@@ -1,7 +1,9 @@
 import express from "express";
-import { updateAllHoldings, updateAllHistory } from "../controllers/recency";
+import { updateAllHoldings, updateAllHistory, getMySubRecencyTradingHistory } from "../controllers/recency";
+import requireAuthMiddleware from "../middleware/requireAuthMiddleware";
 
 export default (router: express.Router) => {
   router.get("/recency/updateAllHoldings", updateAllHoldings);
   router.get("/recency/updateAllHistory", updateAllHistory);
+  router.get("/recency/getMyRecencyTradingHistory", getMySubRecencyTradingHistory);
 };
