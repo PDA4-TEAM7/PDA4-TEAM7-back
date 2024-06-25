3b8b67894eca52c709892530e633eab964161fa5
soyeon <uyyu1734@gmail.com>
2024-06-25 19:33:18 +0900

    feat: trading history api추가

--- src/controllers/tradeHistoryAPI.ts
+++ src/controllers/tradeHistoryAPI.ts
@@ -1,29 +1,19 @@
-import express from "express";
 import { Trading_history } from "../models/trading_history"; // 모델 파일 경로에 맞게 수정
 import { Request, Response } from "express";
 import { Stock_in_account } from "../models/stock_in_account";
+import { Stock } from "../models/stock";
 
 export const tradingHistory = async (req: Request, res: Response) => {
-  const { account_id } = req.body;
-
+  const { accountId } = req.params;
   try {
     const tradingHistory = await Trading_history.findAll({
       include: [
         {
-          model: Stock_in_account,
-          attributes: ["pchs_amt", "evlu_pfls_amt"],
+          model: Stock,
+          required: true,
         },
       ],
-      where: { account_id: account_id },
-      attributes: [
-        "trading_id",
-        "account_id",
-        "stock_id",
-        "sll_buy_dvsn_cd",
-        "trade_dt",
-        "tot_ccld_qty",
-        "tot_ccld_amt",
-      ],
+      where: { account_id: accountId },
     });
 
     console.log(tradingHistory);
